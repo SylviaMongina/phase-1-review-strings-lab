acbfc83ee0e1ab7c568bd38180cb1199e9eddd37
SylviaMonginaBosire <sylvia.bosire@student.moringaschool.com>
2022-11-21 18:37:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = 'Sylvia Bosire';
+//const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+//const shortGreeting = 'Welcome, ';
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
